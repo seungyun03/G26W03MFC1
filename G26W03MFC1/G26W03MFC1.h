8daf9c1088877c3c1d418e94ed3cf905f4fbd6e9
seungyun03 <neoyoon04@gmail.com>
2026-09-17 15:44:10 +0900

    초기 커밋.

---
+++ G26W03MFC1/G26W03MFC1.h
@@ -0,0 +1,33 @@
+﻿
+// G26W03MFC1.h: G26W03MFC1 애플리케이션의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CG26W03MFC1App:
+// 이 클래스의 구현에 대해서는 G26W03MFC1.cpp을(를) 참조하세요.
+//
+
+class CG26W03MFC1App : public CWinApp
+{
+public:
+	CG26W03MFC1App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CG26W03MFC1App theApp;
---
+++ G26W03MFC1/G26W03MFC1View.h
@@ -0,0 +1,49 @@
+﻿
+// G26W03MFC1View.h: CG26W03MFC1View 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CG26W03MFC1View : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CG26W03MFC1View() noexcept;
+	DECLARE_DYNCREATE(CG26W03MFC1View)
+
+// 특성입니다.
+public:
+	CG26W03MFC1Doc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CG26W03MFC1View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // G26W03MFC1View.cpp의 디버그 버전
+inline CG26W03MFC1Doc* CG26W03MFC1View::GetDocument() const
+   { return reinterpret_cast<CG26W03MFC1Doc*>(m_pDocument); }
+#endif
+
